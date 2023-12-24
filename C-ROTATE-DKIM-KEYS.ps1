7efa33ae5243145d817fa0ef45c65930a4e97374
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2023-12-24 21:51:10 +0100

    Update C-ROTATE-DKIM-KEYS.ps1

--- C-ROTATE-DKIM-KEYS.ps1
+++ C-ROTATE-DKIM-KEYS.ps1
@@ -1,5 +1,9 @@
 #Connect EXO
 Connect-ExchangeOnline -ManagedIdentity -Organization yourorg.onmicrosoft.com
 
-#Rotatge the DKIM key
-Rotate-DkimSigningConfig -KeySize 2048 -Identity "yourdomain.com"
+#Rotate the DKIM keys for all enabled domains
+$Domains = (Get-DkimSigningConfig | Where-Object {$_.Enabled -eq 'True'}).domain
+
+foreach ($domain in $domains){
+    Rotate-DkimSigningConfig -KeySize 2048 -Identity $domain
+}
