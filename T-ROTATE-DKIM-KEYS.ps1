3a2e58a2985e7ce9e5612b052bdcf92b8f90e7b7
Ricardo van der Linden <96340723+vand3rlinden@users.noreply.github.com>
2025-06-13 11:56:25 +0200

    Update T-ROTATE-DKIM-KEYS.ps1

--- T-ROTATE-DKIM-KEYS.ps1
+++ T-ROTATE-DKIM-KEYS.ps1
@@ -1,5 +1,5 @@
 #Connect EXO
-Connect-ExchangeOnline -ManagedIdentity -Organization yourorg.onmicrosoft.com
+Connect-ExchangeOnline -ManagedIdentity -Organization yourorg.onmicrosoft.com #Add your organization's MOERA domain
 
 #Rotate the DKIM keys for all enabled domains
 $Domains = (Get-DkimSigningConfig | Where-Object {$_.Enabled -eq 'True'}).domain
